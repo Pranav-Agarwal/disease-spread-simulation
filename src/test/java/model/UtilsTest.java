d300e26bea6df7978c5e28111205ab0263c63149
JyotiV <jyotivaswani2@gmail.com>
2020-12-09 01:05:34 +0530

    Utils unit test cases

---
+++ src/test/java/model/UtilsTest.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+
+class UtilsTest {
+
+	@Test
+	void TestUpdate1() {
+		
+		List<Location> locs1 = new ArrayList<>();
+		locs1.add(new Location(1,1));
+		locs1.add(new Location(5,6));
+		
+		List<Location> locs2 = new ArrayList<>();
+		locs2.add(new Location(2,2));
+		locs2.add(new Location(5,6));
+		Building b1 = new OfficeBuilding(locs1);
+		Building b2 = new OfficeBuilding(locs2);
+		Assertions.assertEquals(1,Utils.getDistance(b1,b2));
+	}
+	
+	@Test
+	void TestUpdate2() {
+
+		List<Location> locs1 = new ArrayList<>();		
+		locs1.add(new Location(1,4));
+		locs1.add(new Location(1,6));		
+		List<Location> locs2 = new ArrayList<>();
+		locs2.add(new Location(1,2));
+		locs2.add(new Location(1,3));
+		Building b1 = new HouseBuilding(locs1);
+		Building b2 = new HouseBuilding(locs2);
+		Assertions.assertEquals(2,Utils.getDistance(b1,b2));
+	}
+
+
+	@Test
+	void TestUpdate3() {
+		List<Location> locs1 = new ArrayList<>();		
+		locs1.add(new Location(1,4));
+		locs1.add(new Location(1,6));		
+		List<Location> locs2 = new ArrayList<>();
+		locs2.add(new Location(1,4));
+		locs2.add(new Location(1,3));
+		Building b1 = new HouseBuilding(locs1);
+		Building b2 = new HouseBuilding(locs2);
+		Assertions.assertEquals(0,Utils.getDistance(b1,b2));
+	}
+	
+	@Test
+	void TestUpdate4() {
+		List<Location> locs1 = new ArrayList<>();		
+		locs1.add(new Location(1,4));		
+		List<Location> locs2 = new ArrayList<>();
+		locs2.add(new Location(1,4));
+		Building b1 = new HouseBuilding(locs1);
+		Building b2 = new OfficeBuilding(locs2);
+		
+		Person p1 = new Person(b1,b2);
+		Person p2 = new Person(b1,b2);
+		
+		Assertions.assertEquals(0,Utils.getDistance(p1, p2));
+		
+
+	}
+	
+/**
+ * 	@Test
+ 
+	void TestUpdate5() {
+		List<Location> locs1 = new ArrayList<>();		
+		locs1.add(new Location(1,6));		
+		List<Location> locs2 = new ArrayList<>();
+		locs2.add(new Location(1,4));
+		Building b1 = new HouseBuilding(locs1);
+		Building b2 = new OfficeBuilding(locs2);
+		
+		Person p1 = new Person(b1,b2);
+		Person p2 = new Person(b1,b2);
+		
+		Assertions.assertEquals(2,Utils.getDistance(p1, p2));
+	}
+	*/
+	
+}
